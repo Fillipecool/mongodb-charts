94ff2338bff3a66a5efd6601aa94a0853482c389
Fillipe Oliveira <91267425+Fillipecool@users.noreply.github.com>
2025-04-10 19:30:02 -0300

    Fix: #1

--- stats-template.js
+++ stats-template.js
@@ -66,7 +66,6 @@ module.exports = function createSVG(data) {
           text-anchor: middle;
         }
       </style>
-
       <rect class="bg" width="100%" height="100%" rx="12"/>
       <text x="30" y="40" class="title">${data.username}'s GitHub Stats</text>
 
